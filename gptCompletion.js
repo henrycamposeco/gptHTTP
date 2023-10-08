46fb14ff3f263656289a9700a2acf01d7d8600fa
Henry.Reyes <henrycamposeco@gmail.com>
2023-10-08 12:45:18 -0600

    chore: change parameter name

--- gptCompletion.js
+++ gptCompletion.js
@@ -2,9 +2,9 @@ import fs from "fs/promises";
 import {createCompletion, loadModel} from "gpt4all";
 import {DEFAULT_PERSONA, DEFAULT_PROMPT, LOAD_MODEL_CONFIGURATION, MODEL_NAME} from "./constants.js";
 
-async function doComplete(file) {
+async function doComplete(persona, prompt) {
     const model = await loadModel(MODEL_NAME, LOAD_MODEL_CONFIGURATION);
-    const filePath = `./personas/${file}.txt`
+    const filePath = `./personas/${persona}.txt`
     try {
         model.config.promptTemplate = await fs.readFile(filePath, 'utf8');
         return createCompletion(model, [{
@@ -18,7 +18,7 @@ async function doComplete(file) {
 
 const generateCompletion = async (prompt = DEFAULT_PROMPT, persona = DEFAULT_PERSONA) => {
     try {
-        return doComplete(persona);
+        return doComplete(persona, prompt);
     } catch (err) {
         console.error("Error reading file:", err);
         return {error: "An error occurred"};
